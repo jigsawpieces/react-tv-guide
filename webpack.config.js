f187a6b6a5afc2a4a22bcd244976915bf6e1cdc0
Kathie <hellokathiewu@gmail.com>
2017-10-23 09:30:50 +0100

    webpack sass

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,8 @@ module.exports = {
           plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
         }
       },
-      { test: /\.css$/, loader: "style-loader!css-loader" }
+      { test: /\.css$/, loader: "style-loader!css-loader" },
+      { test: /\.scss$/, loaders: ['style', 'css', 'sass']}
     ],
   },
   output: {
